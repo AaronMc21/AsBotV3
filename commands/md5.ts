6a8582d6ce6d08c95d840badad8ccc818dd94d0f
AaronMc21 <aaron.mcnulty@outlook.com>
2022-06-21 12:14:03 +0100

    formatting: changed space indents to tabs

--- commands/md5.ts
+++ commands/md5.ts
@@ -5,10 +5,10 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('md5')
 		.setDescription('Hashes provided string in MD5')
-        .addStringOption(option =>
-            option.setName('input')
-                .setDescription('test')
-                .setRequired(true)),
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('Input to be hashed.')
+				.setRequired(true)),
 
 	async execute(interaction) {
 		await interaction.reply(crypt.createHash('md5').update(interaction.options.getString('input')).digest("hex"));
