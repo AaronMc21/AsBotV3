7e53d5c72b27478df4febcee486846fc357c83c1
AaronMc21 <aaron.mcnulty@outlook.com>
2022-06-24 13:37:20 +0100

    formatting: changed space indents to tabs

--- commands/hug.ts
+++ commands/hug.ts
@@ -1,14 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('hug')
-        .setDescription('Hugs mentioned user.')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('User to hug.')
-                .setRequired(true)),
-    async execute(interaction) {
-        await interaction.reply(`${interaction.options.getUser('user').toString()}, you have been hugged by ${interaction.user.username}! :people_hugging: `);
-    },
+	data: new SlashCommandBuilder()
+		.setName('hug')
+		.setDescription('Hugs mentioned user.')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('User to hug.')
+				.setRequired(true)),
+	async execute(interaction) {
+		await interaction.reply(`${interaction.options.getUser('user').toString()}, you have been hugged by ${interaction.user.username}! :people_hugging: `);
+	},
 };
