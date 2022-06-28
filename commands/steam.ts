d8d30afa96ec2b4b46d60f4d4b67d06428237bca
AaronMc21 <aaron.mcnulty@outlook.com>
2022-06-28 10:16:34 +0100

    bugfix: DiscordJS subcommand name formatting

--- commands/steam.ts
+++ commands/steam.ts
@@ -5,28 +5,28 @@ module.exports = {
 		.setName('steam')
 		.setDescription('WIP - Gets Steam user info')
 		.addSubcommand(subcommand =>
-			subcommand.setName('SteamID')
+			subcommand.setName('steamid')
 				.setDescription('Gets Steam user info from SteamID')
 				.addStringOption(option =>
 					option.setName('input')
 						.setDescription('SteamID to get info from.')
 						.setRequired(true)))
 		.addSubcommand(subcommand =>
-			subcommand.setName('SteamID3')
+			subcommand.setName('steamid3')
 				.setDescription('Gets Steam user info from SteamID3')
 				.addStringOption(option =>
 					option.setName('input')
 						.setDescription('SteamID3 to get info from.')
 						.setRequired(true)))
 		.addSubcommand(subcommand =>
-			subcommand.setName('SteamID64')
+			subcommand.setName('steamid64')
 				.setDescription('Gets Steam user info from SteamID64')
 				.addStringOption(option =>
 					option.setName('input')
 						.setDescription('SteamID64 to get info from.')
 						.setRequired(true)))
 		.addSubcommand(subcommand =>
-			subcommand.setName('CustomURL')
+			subcommand.setName('customurl')
 				.setDescription('Gets Steam user info from CustomURL')
 				.addStringOption(option =>
 					option.setName('input')
@@ -34,13 +34,13 @@ module.exports = {
 						.setRequired(true))),
 	
 	async execute(interaction) {
-		if (interaction.options.getSubcommand() === 'SteamID') {
+		if (interaction.options.getSubcommand() === 'steamid') {
 			await interaction.reply(`SteamID: ${interaction.options.getString('input')}`);
-		} else if (interaction.options.getSubcommand() === 'SteamID3') {
+		} else if (interaction.options.getSubcommand() === 'steamid3') {
 			await interaction.reply(`SteamID3: ${interaction.options.getString('input')}`);
-		} else if (interaction.options.getSubcommand() === 'SteamID64') {
+		} else if (interaction.options.getSubcommand() === 'steamid64') {
 			await interaction.reply(`SteamID64: ${interaction.options.getString('input')}`);
-		} else if (interaction.options.getSubcommand() === 'CustomURL') {
+		} else if (interaction.options.getSubcommand() === 'customurl') {
 			await interaction.reply(`CustomURL: ${interaction.options.getString('input')}`);
 		}
 	},
